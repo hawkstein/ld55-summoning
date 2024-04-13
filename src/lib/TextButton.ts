0f799ea7caa748f87f109519cab14a1726c9aad3
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2024-04-13 12:54:16 +0100

    feat: add TextButton

---
+++ src/lib/TextButton.ts
@@ -0,0 +1,68 @@
+import Phaser from "phaser"
+import { PaletteNum } from "./Palette"
+import { TEAL_16 } from "./BitmapFontKey"
+
+export default class TextButton extends Phaser.GameObjects.Container {
+  private background: Phaser.GameObjects.Rectangle
+  private text: Phaser.GameObjects.BitmapText
+  private callback: () => void
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    text: string,
+    callback: () => void
+  ) {
+    super(scene, x, y)
+
+    this.on("pointerup", this.onButtonUp, this)
+    this.on("pointerover", this.onButtonOver, this)
+    this.on("pointerout", this.onButtonOut, this)
+
+    this.text = scene.make
+      .bitmapText({
+        text,
+        x: 0,
+        y: 0,
+        font: TEAL_16,
+      })
+      .setOrigin(0.5)
+      .setScale(2)
+
+    this.background = scene.add
+      .rectangle(
+        0,
+        0,
+        this.text.width + 40,
+        this.text.height + 20,
+        PaletteNum.HotPanda.Red
+      )
+      .setOrigin(0.5)
+    this.setInteractive({
+      hitArea: this.background.getBounds(),
+      hitAreaCallback: Phaser.Geom.Rectangle.Contains,
+      useHandCursor: true,
+    })
+
+    this.add(this.background)
+    this.add(this.text)
+
+    this.callback = callback
+
+    scene.add.existing(this)
+  }
+
+  private onButtonOver() {
+    this.background.fillColor = PaletteNum.HotPanda.Orange
+  }
+
+  private onButtonOut() {
+    this.background.fillColor = PaletteNum.HotPanda.Red
+  }
+
+  private onButtonUp() {
+    this.background.fillColor = PaletteNum.HotPanda.Red
+    this.callback()
+  }
+}
