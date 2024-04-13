49d9bb2afd90c9a5a535a39ac30a26f43a3323c5
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2024-04-13 11:17:34 +0100

    feat: add HotPanda palette

---
+++ src/lib/Palette.ts
@@ -0,0 +1,38 @@
+export const PaletteNum = {
+  HotPanda: {
+    Red: 0xc8024f,
+    Orange: 0xff585f,
+    Yellow: 0xfce4a8,
+    Slate: 0xbed7e9,
+    Green: 0x0a7e8b,
+    DarkGreen: 0x093a54,
+    DarkBlue: 0x2f2041,
+    Background: 0x10061d,
+  },
+} as const
+
+export const PaletteHash = {
+  HotPanda: {
+    Red: "#c8024f",
+    Orange: "#ff585f",
+    Yellow: "#fce4a8",
+    Slate: "#bed7e9",
+    Green: "#0a7e8b",
+    DarkGreen: "#093a54",
+    DarkBlue: "#2f2041",
+    Background: "#10061d",
+  },
+} as const
+
+export const PaletteRGB = {
+  HotPanda: {
+    Red: { r: 200, g: 2, b: 79 },
+    Orange: { r: 255, g: 88, b: 95 },
+    Yellow: { r: 252, g: 228, b: 168 },
+    Slate: { r: 190, g: 215, b: 233 },
+    Green: { r: 10, g: 126, b: 139 },
+    DarkGreen: { r: 9, g: 58, b: 84 },
+    DarkBlue: { r: 47, g: 32, b: 65 },
+    Background: { r: 16, g: 6, b: 29 },
+  },
+} as const
